2fd99299b05e811858f561f2d01b1a0c25e268dc
Jay Sabva <202101224@daiict.ac.in>
2025-02-02 22:52:40 +0530

    🚀[Professor Service Impl - Code Cleaning]

--- src/main/java/org/jaysabva/woc_crs/service/Implementation/ProfessorServiceImplementation.java
+++ src/main/java/org/jaysabva/woc_crs/service/Implementation/ProfessorServiceImplementation.java
@@ -1,28 +1,26 @@
 package org.jaysabva.woc_crs.service.Implementation;
 
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.transaction.annotation.Transactional;
 
-import org.jaysabva.woc_crs.service.AdminService;
 import org.jaysabva.woc_crs.service.ProfessorService;
 
 
-import org.jaysabva.woc_crs.entity.Student;
 import org.jaysabva.woc_crs.entity.Professor;
-import org.jaysabva.woc_crs.dto.StudentDto;
-import org.jaysabva.woc_crs.repository.StudentRepository;
 import org.jaysabva.woc_crs.dto.ProfessorDto;
 import org.jaysabva.woc_crs.repository.ProfessorRepository;
-import org.jaysabva.woc_crs.service.StudentService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ProfessorServiceImplementation implements ProfessorService {
 
-    @Autowired
-    private ProfessorRepository professorRepository;
+    private final ProfessorRepository professorRepository;
 
+    @Autowired
     public ProfessorServiceImplementation(ProfessorRepository professorRepository){
         this.professorRepository = professorRepository;
     }
@@ -32,11 +30,11 @@ public class ProfessorServiceImplementation implements ProfessorService {
 
         Professor professor = professorRepository.findByEmail(email);
         if(professor == null){
-            throw new IllegalArgumentException("Professor with this email does not exist");
+            throw new EntityNotFoundException("Professor with this email does not exist");
         }
 
         if(professorRepository.findByEmail(professorDto.email()) != null) {
-            throw new IllegalArgumentException("Professor with this email already exists");
+            throw new EntityExistsException("Professor with this email already exists");
         }
 
         professor.setName(professorDto.name());
@@ -45,7 +43,7 @@ public class ProfessorServiceImplementation implements ProfessorService {
 
         try {
             professorRepository.save(professor);
-        } catch (DataIntegrityViolationException e) {
+        } catch (Exception e) {
             throw new IllegalArgumentException("Failed to update professor due to database constraints");
         }
 
@@ -53,12 +51,12 @@ public class ProfessorServiceImplementation implements ProfessorService {
     }
 
     @Override
-    public java.util.Map<String, String> getProfessor(String email){
+    public Map<String, String> getProfessor(String email){
         Professor professor = professorRepository.findByEmail(email);
         if(professor == null){
-            throw new IllegalArgumentException("Professor with this email does not exist");
+            throw new EntityNotFoundException("Professor with this email does not exist");
         }
-        return new java.util.HashMap<String, String>(){{
+        return new HashMap<> (){{
             put("name", professor.getName());
             put("email", professor.getEmail());
         }};
