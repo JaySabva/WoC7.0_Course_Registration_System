e1812430ee71c421d83ec9cbf0798143ebea3df3
Jay Sabva <202101224@daiict.ac.in>
2025-02-04 10:50:12 +0530

    🚀[Swagger UI Error Fixed]

--- src/main/java/org/jaysabva/woc_crs/config/Swagger.java
+++ src/main/java/org/jaysabva/woc_crs/config/Swagger.java
@@ -2,16 +2,26 @@ package org.jaysabva.woc_crs.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.Arrays;
 
 @Configuration
 public class Swagger {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(
-                new Info().title("Course Registration System").description("Jay Sabva")
-        );
+        return new OpenAPI()
+                .openapi("3.0.0")
+                .info(new Info().title("Course Registration System").description("Jay Sabva"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .security(Arrays.asList(
+                        new SecurityRequirement().addList("bearerAuth")));
     }
 }
